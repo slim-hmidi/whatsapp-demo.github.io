e3e7cdbc31b3f7c7a1ea47e976b681dfc4e23192
Slim Hmidi <slim.hmidi1@gmail.com>
2022-07-05 10:54:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+export default function App() {
+  const [value, setValue] = React.useState("");
+  const handleClick = () => {
+    if (value) {
+      const text = value;
+      return window.open(
+        `https://api.whatsapp.com/send?text=${text}`,
+        "_blank"
+      );
+    }
+  };
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '0 auto'}}>
+      <h3>Share your message on whatsapp</h3>
+      <input type="text" name="message" onChange={handleChange} style={{marginRight: 10}}/>
+      <button onClick={handleClick} className="mobileShow">
+        Share to whatsapp
+      </button>
     </div>
   );
 }
-
-export default App;
